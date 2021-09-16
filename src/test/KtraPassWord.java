e2425b2e737d9e0a0558ac24307946c8a32dbc3e
Admin <Admin@Admin>
2021-09-16 10:49:10 +0700

    ktra password

---
+++ src/test/KtraPassWord.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Scanner;
+public class KtraPassWord {
+	
+	    public static void main(String args[])
+	    {
+	        String username, password;
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Nhập username:");
+	        username = s.nextLine();
+	        System.out.print("Nhập password:");
+	        password = s.nextLine();
+	        if(username.equals("HuyHoang") && password.equals("2021"))
+	        {
+	            System.out.println("Đăng nhập thành công");
+	        }
+	        else
+	        {
+	            System.out.println("Đăng nhập thất bại,hãy kiểm tra lại tài khoản hoặc mật khẩu của bạn");
+	        }
+	    }
+	}
+
