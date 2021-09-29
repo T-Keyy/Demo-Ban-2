6b5794e50db5815420eb483eb73eecedf0d9e1f9
Admin <Admin@Admin>
2021-09-29 15:40:52 +0700

    COMIit

---
+++ src/Pass2/InSangMet.java
@@ -0,0 +1,17 @@
+package Pass2;
+
+import java.util.Scanner;
+
+public class InSangMet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner input = new Scanner(System.in);
+
+	        System.out.print("Nhap : ");
+	        double inch = input.nextDouble();
+
+	        double  met =inch * 0.0254;
+	        System.out.println(inch + "Inch la " + met + " Met");
+	    }
+	}
