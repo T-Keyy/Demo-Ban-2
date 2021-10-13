f56eac6cc28f216e461012be1c0ce1474b5657e6
Admin <Admin@Admin>
2021-10-13 19:30:38 +0700

    Mang

---
+++ src/mang/MangTBC.java
@@ -0,0 +1,39 @@
+package mang;
+
+import java.util.Scanner;
+
+public class MangTBC {
+	public static void InputArray(int []arr) {
+		Scanner scanner = new Scanner(System.in);
+		for (int i=0; i<=arr.length-1; i++) {
+			System.out.print("a[" + i + "] = ");
+			arr[i] = scanner.nextInt();
+			
+		}
+		scanner.close();
+	}
+	public static void Average(int []arr) {
+		double average = 0;
+		for(int i=0; i<=arr.length-1; i++) {
+			average += arr[i];
+		}
+		System.out.println("Average of array is " + (average / arr.length));
+	}
+	
+	public static void OutputArray(int []arr) {
+		for (int i=0; i<=arr.length-1; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+		public static void main(String[] args) {
+	        // TODO Auto-generated method stub
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Nhap Mang: ");
+	        int n = scanner.nextInt();
+	        int []arr = new int[n];
+	        InputArray(arr);
+	        OutputArray(arr);
+	        System.out.println();
+	        Average(arr);
+	    }
+	}
